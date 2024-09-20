0d1cf5b755911800d77357bff0b5ebabcfcd3019
Robson002886 <sayderbr@gmail.com>
2024-09-19 21:29:38 -0300

    Update javinha.js

--- js/javinha.js
+++ js/javinha.js
@@ -1,4 +1,12 @@
 function calcular(){
-alert("ok");
-
+    var cliente = 
+    document.getElementById("cxnome") .value;
+    var placa =
+    doxument.getElementById("cxplaca") .value;
+    var carro =
+    document.getElementById("cxcarro") .value;
+    var dias =
+    document.getElementById("cxdias") .value;
+    var valor =
+    document.getElementById("cxvalor") .value;
 }
