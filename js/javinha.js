f8d209d6bcde10fe0544ddd96d463d208c96ef6f
Robson002886 <sayderbr@gmail.com>
2024-09-23 22:11:50 -0300

    aula 4

--- js/javinha.js
+++ js/javinha.js
@@ -1,19 +1,24 @@
-function calcular(){
-    var cliente = 
-    document.getElementById("cxnome") .value;
-    var placa =
-    doxument.getElementById("cxplaca") .value;
-    var carro =
-    document.getElementById("cxcarro") .value;
-    var dias =
-    document.getElementById("cxdias") .value;
-    var valor =
-    document.getElementById("cxvalor") .value;
+function calcular() {
+    var cliente = document.getElementById("cxnome").value;
 
-    var bruto = parsefloat(valor)*parseint(dias);
-    if(bruto > 150){
-        var total = bruto - (bruto * 0.10);
-        alert("total" + total)
+    var placa = document.getElementById("cxplaca").value;
+
+    var carro = document.getElementById("cxcarro").value;
+
+    var dias = document.getElementById("cxdias").value;
+
+    var valor = document.getElementById("cxvalor").value;
+
+    var bruto = parseFloat(valor) * parseInt(dias);
+
+    var mensagem = `Cliente: ${cliente}\nPlaca: ${placa}\nCarro: ${carro}\nDias de estadia: ${dias}\n`;
+
+   
+    if (dias >= 150) {
+        var total = bruto - (bruto * 0.10); 
+        mensagem += `\nVocê recebeu um desconto de 10%!\nValor total sem desconto: R$ ${bruto.toFixed(2)}\nValor total com desconto: R$ ${total.toFixed(2)}`;
+    } else {
+        mensagem += `\nValor total a pagar: R$ ${bruto.toFixed(2)}`;
     }
-    else{ var total = bruto
-    }
+    alert(mensagem);
+}
